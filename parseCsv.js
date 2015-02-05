fe5bd5ecf2756eec83e75fedfc2a54d2346f4d42
Andrew Kim <akim@yp.com>
2015-02-05 13:53:19 -0800

    Add party size and remove dups

--- parseCsv.js
+++ parseCsv.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
+var _ = require('underscore');
 
 
 var str = fs.readFileSync("seating_chart.csv").toString();
 var rows = str.split(/\n/);
-var guests = [];
+var guests = {};
 var tableNums;
+var name;
 
 for (var i = 0; i < rows.length; i++) {
     var cells = rows[i].split(",");
@@ -12,13 +14,18 @@ for (var i = 0; i < rows.length; i++) {
         tableNums = cells;
     } else {
         for (var j = 0; j < cells.length; j ++) {
-            guests.push({
-                name: cells[j],
-                table: tableNums[j]
-            });
+            name = cells[j];
+            if (guests[name]) {
+                guests[name].count++;
+            } else {
+                guests[name] = {
+                    name: cells[j],
+                    table: tableNums[j],
+                    count: 1
+                };
+            }
         }
     }
 }
 
-console.log(guests);
-
+console.log(_.values(guests));
