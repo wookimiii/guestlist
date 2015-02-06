20d192409cd58bee887105dbf7daaa0ff2c61f01
Andrew Kim <akim@yp.com>
2015-02-05 21:45:26 -0800

    Highlight tables on click

--- public/search.js
+++ public/search.js
@@ -8,7 +8,7 @@ function renderGuests(guests) {
 }
 
 $(function () {
-    guestRowTemplate = _.template("<tr><td><%= name %></td><td><%= table %></td></tr>");
+    guestRowTemplate = _.template("<tr><td><%= name %></td><td class='tableNum'><%= table %></td></tr>");
     $guestList = $(".guestList tbody");
     $search = $("#search");
 
@@ -21,11 +21,13 @@ $(function () {
     });
 
     bindFocus();
+
+    $("#floorplan").on("click", ".close", showList);
+    $("#list tbody").on("click", "tr", showFloorplan);
 });
 
 function searchName(guests, term) {
     term = term.toLowerCase().replace(" ", ".+");
-    console.log(term);
     var regTerm = new RegExp(term, "i");
 
     function predicate (guest) {
@@ -44,7 +46,101 @@ function showHeader() {
 }
 
 function bindFocus() {
-    $search.on('focusin', hideHeader);
-
-    $search.on('focusout', showHeader);
+    // $search.on('focusin', hideHeader);
+    // $search.on('focusout', showHeader);
 }
+
+function showFloorplan(e) {
+    $("#list").hide();
+    $("#floorplan").show();
+    
+    highlightTable(tableNum(e));
+}
+
+function tableNum(e) {
+    return parseInt($(e.currentTarget).find(".tableNum").html());
+}
+
+function showList() {
+    $("#floorplan").hide();
+    $("#list").show();
+}
+
+var HEIGHT_PERC = 0.15;
+var WIDTH_PERC = 0.10;
+function highlightWidth() {
+    var $image = $("#floorplan img");
+    return $image.width() * WIDTH_PERC;
+}
+
+function highlightHeight() {
+    var $image = $("#floorplan img");
+    return $image.height() * HEIGHT_PERC;
+}
+
+function highlightTable(num) {
+    var $highlight = $("#table-highlight");
+    var $image = $("#floorplan img");
+
+    var pos = calcHighlightPosition(num);
+    $highlight.css({
+        height: highlightHeight(),
+        width: highlightWidth(),
+        top: pos[0],
+        left: pos[1]
+    }); 
+}
+
+function calcHighlightPosition(num) {
+    var $image = $("#floorplan img");
+    var pos = POSITIONS[num];
+    var hWidth = highlightWidth();
+    var hHeight = highlightHeight();
+    var hLeft = (pos[0] * hWidth) + hWidth/3.2;
+    if (num < 16) {
+        hLeft = hLeft + 2.5 * hWidth;
+    }
+    var hTop = pos[1] * highlightHeight();
+    hTop = hTop + $image.position().top + (highlightHeight() * 1.25);
+
+    return [hTop, hLeft];
+}
+
+var POSITIONS = {
+    1: [6,0],
+    2: [6,1],
+    3: [6,2],
+    4: [6,3],
+    5: [6,4],
+    6: [5,0],
+    7: [5,1],
+    8: [5,2],
+    9: [5,3],
+    10: [5,4],
+    11: [4,0],
+    12: [4,1],
+    13: [4,2],
+    14: [4,3],
+    15: [4,4],
+    16: [3,0],
+    17: [3,1],
+    18: [3,2],
+    19: [3,3],
+    20: [3,4],
+    21: [2,0],
+    22: [2,1],
+    23: [2,2],
+    24: [2,3],
+    25: [2,4],
+    26: [1,0],
+    27: [1,1],
+    28: [1,2],
+    29: [1,3],
+    30: [1,4],
+    31: [0,0],
+    32: [0,1],
+    33: [0,2],
+    34: [0,3],
+    35: [0,4]
+};
+
